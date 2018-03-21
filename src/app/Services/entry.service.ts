3e3ea5f51517bd6ceaf3032012a03fd1d34f7570
Szymon Bakunowicz <zorzin0os@gmail.com>
2018-03-22 00:23:39 +0100

    Entry service api methods

--- src/app/Services/entry.service.ts
+++ src/app/Services/entry.service.ts
@@ -14,29 +14,70 @@ export class EntryService {
               private apiSerive: ApiService,
               private userService: UserService) { }
 
+  private AddTokenToHeaders()
+  {
+    if(!this.headers.has("Authorization"))
+    {
+      let authToken = localStorage.getItem('auth_token');
+      this.headers.append('Authorization', `Bearer ${authToken}`);
+    }
+  }
+
   public SaveEntry(startTime, endTime, date:Date, outsiteTime, type:string,allDay:boolean)
   {
-      let entry = new Entry();
-      entry.AllDay = allDay;
-      entry.StartTime = this.GetTimeStringFromTime(startTime);
-      entry.EndTime = this.GetTimeStringFromTime(endTime);
-      entry.Date = this.GetDateWithOffset(date);
-      entry.OutsideTime = this.GetTimeStringFromTime(outsiteTime);
-      entry.Type = type;
-      entry.UserId = this.userService.getUserId();
+    let entry = this.CreateEntry(-1, allDay, startTime, endTime, date, outsiteTime, type);
+    return this.SendEntryToAPI(entry);
+  }
 
-      this.SendEntryToAPI(entry);
+  public EditEntry(id:number, startTime, endTime, date:Date, outsiteTime, type:string,allDay:boolean)
+  {
+    let entry = this.CreateEntry(id, allDay, startTime, endTime, date, outsiteTime, type);
+    return this.SendEditToAPI(entry);
+  }
+
+  public GetEntryById(id: number)
+  {
+    this.AddTokenToHeaders();
+    return this.http.get(this.apiSerive.GetEntryURL() + "/" + id).toPromise();
+  }
+
+  public GetEntriesForDates(startDate: Date, endDate: Date)
+  {
+    this.AddTokenToHeaders();
+    return this.http.get(this.apiSerive.GetEntryURL() + "/" + startDate + "/" + endDate)
+      .toPromise();
   }
 
   private SendEntryToAPI(entry:Entry)
   {
-    let authToken = localStorage.getItem('auth_token');
-    this.headers.append('Authorization', `Bearer ${authToken}`);
+    this.AddTokenToHeaders();
 
     let body = JSON.stringify(entry);
     console.log(body);
-    this.http.post(this.apiSerive.GetEntryURL(),body,{headers:this.headers,responseType: 'text' })
-      .subscribe();
+    return this.http.post(this.apiSerive.GetEntryURL(),body,{headers:this.headers,responseType: 'text' })
+      .toPromise();
+  }
+
+  private SendEditToAPI(entry: Entry) {
+    this.AddTokenToHeaders();
+
+    let body = JSON.stringify(entry);
+    console.log(body);
+    return this.http.put(this.apiSerive.GetEntryURL() + "/" + entry.Id,body,{headers:this.headers,responseType: 'text' })
+      .toPromise();
+  }
+
+  private CreateEntry(id: number, allDay: boolean, startTime, endTime, date: Date, outsiteTime, type: string) {
+    let entry = new Entry();
+    entry.Id = id;
+    entry.AllDay = allDay;
+    entry.StartTime = this.GetTimeStringFromTime(startTime);
+    entry.EndTime = this.GetTimeStringFromTime(endTime);
+    entry.Date = this.GetDateWithOffset(date);
+    entry.OutsideTime = this.GetTimeStringFromTime(outsiteTime);
+    entry.Type = type;
+    entry.UserId = this.userService.getUserId();
+    return entry;
   }
 
   private GetDateWithOffset(date :Date)
